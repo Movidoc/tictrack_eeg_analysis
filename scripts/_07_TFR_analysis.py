c3f3cfccd78d515c264d68b129629cf756d13c1e
martyna siatka <martyna.siatka@gmail.com>
2026-03-27 18:30:18 +0100

    Time-frequency analysis, theres a problem with cropping no_tic epochs data for now

--- scripts/_07_TFR_analysis.py
+++ scripts/_07_TFR_analysis.py
@@ -38,6 +38,13 @@ def parse_args() -> argparse.Namespace:
         default=None,
         help="Subjects to process, e.g. --sub sub-028 sub-029. Default: all dataset/sub-*",
     )
+
+    parser.add_argument(
+        "--baseline_phase",
+        type=str,
+        default="ALL",
+        help="Which phase to use for baseline (e.g. PHASE_EC, PHASE_EO, PHASE_FREE, or ALL)"
+    )
     return parser.parse_args()
 
 def main():
@@ -53,4 +60,109 @@ def main():
 
     for sub, cfg in subjects.items():
 
-        # --- 1. Load preproccessed data 
+        # --- 1. Load preprocessed epochs data 
+        print(f"\n{'='*60}")
+        print(f"[1/5] Loading epochs data ...")
+        print(f"{'='*60}")
+        # -------------- Pre-tic epochs -----------
+        epochs_fif_path = PREPROC_DIR / sub / "tics_manual"/ "tic" / f"{sub}_ses-01_task-tictrack_tic_epoch.fif"
+        tic_epochs = mne.read_epochs(epochs_fif_path, preload = True, verbose = "ERROR")
+
+        if tic_epochs.metadata is None:
+            print("[ERROR] Epochs must contain metadata with phase and tic_type")
+        phases = tic_epochs.metadata["phase"].unique()
+        tic_types = tic_epochs.metadata["tic_type"].unique()
+
+        print(f"[INFO] Phases: {phases}")
+        print(f"[INFO] Tic types: {tic_types}")
+
+        # ------------- No-tic epochs-----------
+        no_epochs_fif = PREPROC_DIR / sub / "tics_manual" /"no_tic" / f"{sub}_ses-01_task-tictrack_no_tic_epoch.fif"
+        no_tic_epochs = mne.read_epochs(no_epochs_fif, preload = True, verbose = "ERROR")
+
+        print(f"[INFO] Loaded {len(tic_epochs)} tic epochs")
+        print(f"[INFO] Loaded {len(no_tic_epochs)} no-tic epochs")
+
+        # -- 2. Time-Frequency Analysis 
+        print(f"\n{'='*60}")
+        print(f"[2/5] Time-Frequency Analysis ...")
+        print(f"{'='*60}")
+
+        # choose the phase for no_tic epochs 
+        baseline_phase = args.baseline_phase
+        if baseline_phase == "ALL":
+            random_epochs = no_tic_epochs
+        else:
+            if no_tic_epochs.metadata is None:
+                print("[ERROR] No metadata found in no_tic_epochs")
+
+            sel = no_tic_epochs.metadata["phase"] == baseline_phase
+            random_epochs = no_tic_epochs[sel]
+
+            print(f"[INFO] Baseline: {baseline_phase} ({len(random_epochs)} epochs)")
+
+        print(f"\n{'='*60}")
+        print(f"[2/2] Plotting TFR Analysis results...")
+        print(f"{'='*60}")
+
+        for phase in phases:
+            for tic in tic_types: 
+                print(f"\n--- {phase} | {tic} ---")
+                # choose the tic type for each phase
+                select = (
+                    (tic_epochs.metadata["phase"] == phase) & 
+                    (tic_epochs.metadata["tic_type"] == tic)
+                )
+                tic_epochs_sel = tic_epochs[select]
+                if len(tic_epochs_sel) == 0:
+                    print(f"  [SKIP] No epochs for {phase} | {tic}, skipping.")
+                    continue
+                print(f"  {phase} | {tic}: tmin={tic_epochs_sel.tmin}, tmax={tic_epochs_sel.tmax}, n={len(tic_epochs_sel)}")
+                print(f"  pre_tic tmin={tic_epochs_sel.tmin}, tmax={tic_epochs_sel.tmax}")
+                print(f"  random  tmin={random_epochs.tmin},  tmax={random_epochs.tmax}")
+
+                # Compute TFR
+                roi_tfr, freqs, times, n  = tfr_per_ROI_normalized(
+                    patient = cfg, pre_tic_epochs = tic_epochs_sel, 
+                    random_epochs = random_epochs, epoch_type='pre_tic', freqs=TFR_PARAMS["freqs"], normalization =TFR_PARAMS["normalization"] )
+
+            # -- 3. Plotting the TFR results
+                fig = plot_trf_roi(roi_tfr, freqs, times, n, epoch_type='pre_tic', vmin=TFR_PARAMS["vmin"], vmax=TFR_PARAMS["vmax"])
+
+                # save the ouputs 
+                out_dir = PREPROC_DIR / sub / "tfr" / phase
+                out_dir.mkdir(parents=True, exist_ok=True)
+
+                fname = f"{sub}_{phase}_{tic}_tfr.png"
+                fig.savefig(out_dir / fname, dpi=150)
+                plt.close(fig)
+
+        # -- 4. Analysis for no_tic epochs ---
+        roi_tfr_nt, freqs_nt, times_nt, n_nt  = tfr_per_ROI_normalized(
+            patient = cfg, pre_tic_epochs = random_epochs, 
+            random_epochs = random_epochs, epoch_type='random', freqs=TFR_PARAMS["freqs"], normalization =TFR_PARAMS["normalization"] )
+        fig_nt = plot_trf_roi(roi_tfr_nt, freqs_nt, times_nt, n_nt, epoch_type='random', vmin=TFR_PARAMS["vmin"], vmax=TFR_PARAMS["vmax"])
+
+        # save the ouputs 
+        out_dir_nt = PREPROC_DIR / sub / "tfr" / "no_tic"
+        out_dir_nt.mkdir(parents=True, exist_ok=True)
+
+        fname_nt = f"{sub}_baseline_tfr.png"
+        fig_nt.savefig(out_dir_nt / fname_nt, dpi=150)
+        plt.close(fig_nt)
+
+
+
+
+        print(f"[OK] Saved → {fname}")
+    print(f"[OK] Saved → {fname_nt}")
+
+
+    print("\n[DONE] All data saved.")
+
+            
+
+
+if __name__ == "__main__":
+    main()
+       
